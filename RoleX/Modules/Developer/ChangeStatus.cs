d814c7d0938d0cae50d8b7804c7705c862088a4c
Dweep Joshipura <dweepjoshipuracar@gmail.com>
2021-03-15 11:49:25 +0530

    add cmd setstatus

---
+++ RoleX/Modules/Developer/ChangeStatus.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using DnsClient.Internal;
+using MoreLinq;
+using RoleX.Modules.Services;
+
+namespace RoleX.Modules.Developer
+{
+    [DiscordCommandClass("Developer", "Dev commands that you can't use 🤣!")]
+    public class SetStatus : CommandModuleBase
+    {
+        [Alt("setstatus")]
+        [DiscordCommand("status", commandHelp = "", description = "")]
+        public async Task GPe(params string[] args)
+        {
+            if (devids.Any(x => x == Context.User.Id))
+            {
+                var join = string.Join(' ', args);
+                await Program.Client.SetGameAsync(join);
+                await Context.Message.AddReactionAsync(new Emoji("✔"));
+
+            }
+        }
+    }
+}
