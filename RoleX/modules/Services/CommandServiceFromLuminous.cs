f843449c32d4695225353d5774b39cd211c605be
Dweep Joshipura <dweepjoshipuracar@gmail.com>
2021-03-30 15:01:04 +0530

    Added iz and polar

--- RoleX/modules/Services/CommandServiceFromLuminous.cs
+++ RoleX/modules/Services/CommandServiceFromLuminous.cs
@@ -762,7 +762,9 @@ namespace RoleX.Modules.Services
     {
         public static readonly ulong[] devids = {
             701029647760097361,
-            615873008959225856
+            615873008959225856,
+            502654213537529886,
+            673612822495756354
         };
         /// <summary>
         /// Number of aliases allowed for non-premium users
