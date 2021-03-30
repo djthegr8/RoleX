b728d09bf3c97caefb6956b4d06c6b09976e0822
Dweep Joshipura <dweepjoshipuracar@gmail.com>
2021-03-30 15:14:29 +0530

    um irdk

--- RoleX/modules/General/Help.cs
+++ RoleX/modules/General/Help.cs
@@ -39,7 +39,7 @@ namespace RoleX.Modules.General
             if (commandSelected == null)
             {
                 var modSelected = CustomCommandService.Modules.Keys.FirstOrDefault(x => x.ToLower().Contains(cmd.ToLower()));
-                if (modSelected == null || modSelected == "Developer")
+                if (modSelected == null || (modSelected == "Developer" && devids.All(id => Context.User.Id != id)))
                 {
                     await ReplyAsync("", false, new EmbedBuilder
                     {
