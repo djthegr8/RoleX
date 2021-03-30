ca8a0596fc64d88e236215751e5eee6ceeb040f9
Dweep Joshipura <dweepjoshipuracar@gmail.com>
2021-03-30 16:00:20 +0530

    add runcmd i.e. restartrolex

--- RoleX/Modules/Developer/runcmd.cs
+++ RoleX/Modules/Developer/runcmd.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 using RoleX.Modules.Services;
 
 namespace RoleX.Modules.Developer
@@ -8,29 +9,15 @@ namespace RoleX.Modules.Developer
     [DiscordCommandClass("Developer", "Dev commands that you can't use 🤣!")]
     public class RunCmd : CommandModuleBase
     {
+        [Alt("restartrolex")]
         [DiscordCommand("runcmd", commandHelp = "", description = "")]
         public async Task RunCmdCommand(params string[] args)
         {
             var bk = string.Join(' ', args);
             if (devids.Any(x => x == Context.User.Id))
             {
-                var escapedArgs = bk.Replace("\"", "\\\"");
-
-                var process = new Process()
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "/bin/bash",
-                        Arguments = $"-c \"{escapedArgs}\"",
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true,
-                    }
-                };
-                process.Start();
-                var result = await process.StandardOutput.ReadToEndAsync();
-                await process.WaitForExitAsync();
-                await ReplyAsync(result);
+                await ReplyAsync("ok doing tysm");
+                Process.Start(@"..\..\..\..\restartrolex.bat");
             }
         }
     }
