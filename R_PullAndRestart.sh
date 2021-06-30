116227427b6902d5e99a14397495282576bbac8c
DJ <dweepjoshipuracar@gmail.com>
2021-06-30 04:15:01 +0000

    Added newer SysDrawCommon

--- R_PullAndRestart.sh
+++ R_PullAndRestart.sh
@@ -4,7 +4,7 @@ git pull -q
 echo "Changes pulled"
 pm2 stop 0 -s
 echo "Starting build procedure"
-dotnet build -v q
+dotnet build >> /dev/null
 echo "Build successful"
 pm2 restart 0 -s
 echo "Restart successful, Good To Go!"
