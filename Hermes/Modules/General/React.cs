585c32d007fe3fc4d0f8ef8881695b71d0c86a47
DJ <dweepjoshipuracar@gmail.com>
2022-05-24 18:16:37 +0530

    emojis added

--- Hermes/Modules/General/React.cs
+++ Hermes/Modules/General/React.cs
@@ -1,6 +1,6 @@
 using System.Threading.Tasks;
 using Hermes.Modules.Services;
-
+using Discord;
 namespace Hermes.Modules.General
 {
     [DiscordCommandClass("General", "General commands for all!")]
@@ -9,7 +9,8 @@ namespace Hermes.Modules.General
         [DiscordCommand("react", commandHelp = "react <emote>", description = "Reacts with the given emote to replied msg")]
         public async Task RReact(string em, params string[] args)
         {
-            var emote = await GetEmote(em);
+            isemoji = Emote.TryParse(em, out var emoji)
+            var emote = isemoji ? emoji : (await GetEmote(em));
             var mes = Context.Message.ReferencedMessage;
             if (emote == null || mes == null){
                 await ReplyAsync("-_-");
