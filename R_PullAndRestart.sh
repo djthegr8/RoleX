1ed15be8126124de69c55a098be2d524b0a748ff
DJ <dweepjoshipuracar@gmail.com>
2023-02-04 18:17:15 +0530

    Update R_PullAndRestart.sh

--- R_PullAndRestart.sh
+++ R_PullAndRestart.sh
@@ -4,7 +4,7 @@ git -C /home/Hermes/RoleX pull -q
 echo "Changes pulled"
 pm2 stop 0 -s
 echo "Starting build procedure"
-dotnet build /home/Hermes/RoleX/Hermes >> /dev/null
+dotnet build /home/Hermes/RoleX/Hermes
 echo "Build successful"
 pm2 restart 0 --cron "*/20 * * * *" -s
 echo "Restart successful, Good To Go!"
